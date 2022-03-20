cb2446b99960d315a13a6ac7537cff8be65b8ec9
Patrick Dolan <dolanp1992@gmail.com>
2022-03-20 11:39:13 -0700

    add create functionality to patients system
    Co-authored-by: Karl Starkweather <stark13@usa.net>
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>

--- DoctorsOffice/Controllers/PatientsController.cs
+++ DoctorsOffice/Controllers/PatientsController.cs
@@ -1,5 +1,6 @@
 using DoctorsOffice.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,25 @@ namespace DoctorsOffice.Controllers
     public ActionResult Index()
     {
       return View(_db.Patients.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.DoctorId = new SelectList(_db.Doctors, "DoctorId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Patient patient, int DoctorId)
+    {
+      _db.Patients.Add(patient);
+      _db.SaveChanges();
+      if (DoctorId != 0)
+      {
+        _db.DoctorPatients.Add(new DoctorPatient() {DoctorId = DoctorId, PatientId = patient.PatientId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
     }
   }
 }
