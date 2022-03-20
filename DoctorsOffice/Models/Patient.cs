1bea52d937f70254e71aa71d6b3eddbe006d68ab
Patrick Dolan <dolanp1992@gmail.com>
2022-03-20 10:48:50 -0700

    add patient properties and joinentities
    Co-authored-by: Karl Starkweather <stark13@usa.net>
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>

--- DoctorsOffice/Models/Patient.cs
+++ DoctorsOffice/Models/Patient.cs
@@ -1,7 +1,18 @@
+using System;
+using System.Collections.Generic;
 namespace DoctorsOffice.Models
 {
   public class Patient
   {
-    
+    public Patient()
+    {
+      this.JoinEntities = new HashSet<DoctorPatient>();
+    }
+
+    public int PatientId { get; set; }
+    public string Name { get; set; }
+    public DateTime Birthday { get; set; }
+    public virtual ICollection<DoctorPatient> JoinEntities { get; set; }
+  
   }
 }
