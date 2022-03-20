8c227f631b25939235afa0591646fffe67a0848f
Patrick Dolan <dolanp1992@gmail.com>
2022-03-20 11:23:21 -0700

    add index view for patients and link from homepagge
    Co-authored-by: Karl Starkweather <stark13@usa.net>
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>

---
+++ DoctorsOffice/Controllers/PatientsController.cs
@@ -0,0 +1,23 @@
+using DoctorsOffice.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DoctorsOffice.Controllers
+{
+  public class PatientsController : Controller
+  {
+    private readonly DoctorsOfficeContext _db;
+
+    public PatientsController(DoctorsOfficeContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Patients.ToList());
+    }
+  }
+}
