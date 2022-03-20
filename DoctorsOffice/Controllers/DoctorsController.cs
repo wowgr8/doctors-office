9c876b16c1b971545f42a3929cb41396cf2cd6f5
Patrick Dolan <dolanp1992@gmail.com>
2022-03-20 14:54:27 -0700

    add doctors doctors index page and views for changing
    Co-authored-by: Karl Starkweather <stark13@usa.net>
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>

---
+++ DoctorsOffice/Controllers/DoctorsController.cs
@@ -0,0 +1,26 @@
+using DoctorsOffice.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DoctorsOffice.Controllers
+{
+  public class DoctorsController: Controller
+  {
+    private readonly DoctorsOfficeContext _db;
+
+    public DoctorsController(DoctorsOfficeContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Doctors.ToList());
+    }
+
+  }
+
+}
