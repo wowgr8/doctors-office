d0be705a64c944ac3c3744ec1df0266a7875dbd4
Patrick Dolan <dolanp1992@gmail.com>
2022-03-21 18:43:57 -0700

    add doctorspecialty join model
    Co-authored-by: Karl Starkweather <stark13@usa.net>

---
+++ DoctorsOffice/Models/DoctorSpecialty.cs
@@ -0,0 +1,11 @@
+namespace DoctorsOffice.Models
+{
+  public class DoctorSpecialty
+  {
+    public int DoctorSpecialtyId { get; set; }
+    public int DoctorId { get; set; }
+    public int SpecialtyId { get; set; }
+    public virtual Doctor Doctor { get; set; }
+    public virtual Specialty Specialty { get; set; }
+  }
+}
